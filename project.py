fda4b4fbccae2bfb2ac210efbd096983144911cc
Dmitrii Govor <85813394+dgovor@users.noreply.github.com>
2022-04-04 14:04:52 -0700

    Add files via upload

---
+++ project.py
@@ -0,0 +1,48 @@
+# %%
+import cv2
+import numpy as np
+import os
+from matplotlib import pyplot as plt
+import time 
+import mediapipe as mp
+
+mp_holistic = mp.solutions.holistic # Holistic model
+mp_drawing = mp.solutions.drawing_utils # Drawing utilities
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
+    image.flags.writeable = False                  # Image is no longer writeable
+    results = model.process(image)                 # Make prediction
+    image.flags.writeable = True                   # Image is now writeable 
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
+    return image, results
+
+def draw_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS,
+                              mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
+                              mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1))
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
+                              mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1))
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
+                              mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1))
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
+                              mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1))
+
+cap = cv2.VideoCapture(0)
+
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        image, results = mediapipe_detection(frame, holistic)
+        
+        draw_landmarks(image,results)
+
+        cv2.imshow('OpenCV feed', image)
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
