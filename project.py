292c19422feb82296ca3afc757b8d26fd9eeb365
Dmitrii Govor <85813394+dgovor@users.noreply.github.com>
2022-04-04 14:11:38 -0700

    Update project.py

--- project.py
+++ project.py
@@ -6,30 +6,30 @@ from matplotlib import pyplot as plt
 import time 
 import mediapipe as mp
 
-mp_holistic = mp.solutions.holistic # Holistic model
-mp_drawing = mp.solutions.drawing_utils # Drawing utilities
+mp_holistic = mp.solutions.holistic
+mp_drawing = mp.solutions.drawing_utils
 
 def mediapipe_detection(image, model):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
-    image.flags.writeable = False                  # Image is no longer writeable
-    results = model.process(image)                 # Make prediction
-    image.flags.writeable = True                   # Image is now writeable 
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
+    results = model.process(image)
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     return image, results
 
 def draw_landmarks(image, results):
     mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS,
-                              mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
-                              mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1))
+                              mp_drawing.DrawingSpec(color=(0,256,0), thickness=1, circle_radius=1), 
+                              mp_drawing.DrawingSpec(color=(0,256,0), thickness=1, circle_radius=1))
     mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                              mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
-                              mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1))
+                              mp_drawing.DrawingSpec(color=(0,256,0), thickness=2, circle_radius=2), 
+                              mp_drawing.DrawingSpec(color=(0,256,0), thickness=1, circle_radius=1))
     mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                              mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
-                              mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1))
+                              mp_drawing.DrawingSpec(color=(0,256,0), thickness=2, circle_radius=2), 
+                              mp_drawing.DrawingSpec(color=(0,256,0), thickness=1, circle_radius=1))
     mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                              mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
-                              mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1))
+                              mp_drawing.DrawingSpec(color=(0,256,0), thickness=2, circle_radius=2), 
+                              mp_drawing.DrawingSpec(color=(0,256,0), thickness=1, circle_radius=1))
 
 cap = cv2.VideoCapture(0)
 
