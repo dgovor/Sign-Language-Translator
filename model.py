cb8edc26d80cfbd8cc9b8c36bcae23391d1aac21
Dmitrii Govor <85813394+dgovor@users.noreply.github.com>
2022-04-07 15:53:45 -0700

    Update model.py

--- model.py
+++ model.py
@@ -30,3 +30,17 @@ X = np.array(sequences)
 Y = to_categorical(labels).astype(int)
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.10, random_state=34, stratify=Y)
+
+# %%
+tb_callback = TensorBoard(log_dir=os.path.join('Logs'))
+
+model = Sequential()
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(10,126)))
+model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(64, return_sequences=False, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(actions.shape[0], activation='softmax'))
+
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+model.fit(X_train, Y_train, epochs=400, callbacks=[tb_callback])
