e3a5115c9c3b5c91200ba02777d073c7e36f630d
Dmitrii Govor <85813394+dgovor@users.noreply.github.com>
2022-04-12 14:26:01 -0700

    Update model.py

--- model.py
+++ model.py
@@ -1,23 +1,28 @@
 # %%
 import numpy as np
 import os
+import mediapipe as mp
+import cv2
+from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.utils import to_categorical
 from itertools import product
+from sklearn import metrics
+from my_functions import *
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.layers import LSTM, Dense, Conv2D
 from tensorflow.keras.callbacks import TensorBoard
 
 DATA_PATH = os.path.join('Data') 
 actions = np.array(os.listdir(os.path.join(DATA_PATH)))
 no_sequences = 30
-sequence_length = 30
+sequence_length = 10
 
 label_map = {label:num for num, label in enumerate(actions)}
 
 sequences, labels = [], []
-for action, sequence in product(actions,range(no_sequences)):
+for action, sequence in product(actions, range(no_sequences)):
     window = []
     for frame_num in range(sequence_length):
         res = np.load(os.path.join(DATA_PATH, action, str(sequence), "{}.npy".format(frame_num)))
@@ -43,4 +48,4 @@ model.add(Dense(32, activation='relu'))
 model.add(Dense(actions.shape[0], activation='softmax'))
 
 model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
-model.fit(X_train, Y_train, epochs=400, callbacks=[tb_callback])
+model.fit(X_train, Y_train, epochs=100, callbacks=[tb_callback])
