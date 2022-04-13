18e0bca64f66ccbf5868bc14d8eb8da35cd4bade
Dmitrii Govor <85813394+dgovor@users.noreply.github.com>
2022-04-13 14:31:21 -0700

    Update data_collection.py

--- data_collection.py
+++ data_collection.py
@@ -1,61 +1,65 @@
 # %%
-import cv2
-import numpy as np
 import os
-import time 
+import numpy as np
+import cv2
 import mediapipe as mp
 from itertools import product
 from my_functions import *
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.utils import to_categorical
+import keyboard
 
-DATA_PATH = os.path.join('Data') 
+actions = np.array(['a', 'b'])
+sequences = 30
+frames = 10
 
-#actions = np.array(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
+PATH = os.path.join('data')
 
-actions = np.array(['c', 'd'])
-
-no_sequences = 30
-sequence_length = 10
-
-for action in actions: 
-    for sequence in range(no_sequences):
-        try: 
-            os.makedirs(os.path.join(DATA_PATH, action, str(sequence)))
-        except:
-            pass
+for action, sequence in product(actions, range(sequences)):
+    try:
+        os.makedirs(os.path.join(PATH, action, str(sequence)))
+    except:
+        pass
 
 cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot access camera")
+    exit()
 
-with mp.solutions.holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-    for action, sequence, frame_num in product(actions, range(no_sequences), range(sequence_length)):
+with mp.solutions.holistic.Holistic(min_detection_confidence=0.75, min_tracking_confidence=0.75) as holistic:
+    for action, sequence, frame in product(actions, range(sequences), range(frames)):
+        if frame == 0: 
+            while True:
+                if keyboard.is_pressed(' '):
+                    break
+                _, image = cap.read()
 
-        ret, frame = cap.read()
+                results = image_process(image, holistic)
+                draw_landmarks(image, results)
 
-        image, results = mediapipe_detection(frame, holistic)
+                cv2.putText(image, 'Recroding data for the "{}". Sequence number {}.'.format(action, sequence),
+                            (20,20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1, cv2.LINE_AA)
+                cv2.putText(image, 'Pause.', (20,400), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2, cv2.LINE_AA)
+                cv2.putText(image, 'Press "Space" when you are ready.', (20,450), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2, cv2.LINE_AA)
+                cv2.imshow('Camera', image)
+                cv2.waitKey(1)
 
-        draw_landmarks(image, results)
+                if cv2.getWindowProperty('Camera',cv2.WND_PROP_VISIBLE) < 1:
+                    break
+        else:
+            _, image = cap.read()
+            results = image_process(image, holistic)
+            draw_landmarks(image, results)
 
-        cv2.putText(image, 'Collecting frames for the letter "{}". Video Number {}'.format(action, sequence), (15,12), 
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-
-        if frame_num == 0: 
-            cv2.putText(image, 'PAUSE', (15,50), 
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255, 0), 4, cv2.LINE_AA)
-            cv2.putText(image, 'Press any key to continue', (15,80), 
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255, 0), 4, cv2.LINE_AA)
-            cv2.imshow('OpenCV feed', image)
-            cv2.waitKey(0)
-        else: 
-            cv2.imshow('OpenCV feed', image)
+            cv2.putText(image, 'Recroding data for the "{}". Sequence number {}.'.format(action, sequence),
+                        (20,20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1, cv2.LINE_AA)
+            cv2.imshow('Camera', image)
+            cv2.waitKey(1)
         
-        keypoints = extract_keypoints(results)
-        npy_path = os.path.join(DATA_PATH, action, str(sequence), str(frame_num))
+        if cv2.getWindowProperty('Camera',cv2.WND_PROP_VISIBLE) < 1:
+             break
+
+        keypoints = keypoint_extraction(results)
+        npy_path = os.path.join(PATH, action, str(sequence), str(frame))
         np.save(npy_path, keypoints)
 
-        cv2.waitKey(1)
-        if cv2.getWindowProperty('OpenCV feed',cv2.WND_PROP_VISIBLE) < 1:
-            break
-                    
     cap.release()
     cv2.destroyAllWindows()
