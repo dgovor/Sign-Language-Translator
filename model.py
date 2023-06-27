9ca474c52ec5cd6960b4647e5fc27e43794ff3fc
Dmitrii Govor <85813394+dgovor@users.noreply.github.com>
2023-06-26 17:23:52 -0700

    Comments

--- model.py
+++ model.py
@@ -1,4 +1,6 @@
 # %%
+
+# Import necessary libraries
 import numpy as np
 import os
 from sklearn.model_selection import train_test_split
@@ -9,16 +11,23 @@ from sklearn import metrics
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 
+# Set the path to the data directory
 PATH = os.path.join('data')
 
+# Create an array of actions (signs) labels by listing the contents of the data directory
 actions = np.array(os.listdir(PATH))
+
+# Define the number of sequences and frames
 sequences = 30
 frames = 10
 
+# Create a label map to map each action label to a numeric value
 label_map = {label:num for num, label in enumerate(actions)}
 
+# Initialize empty lists to store landmarks and labels
 landmarks, labels = [], []
 
+# Iterate over actions and sequences to load landmarks and corresponding labels
 for action, sequence in product(actions, range(sequences)):
     temp = []
     for frame in range(frames):
@@ -27,10 +36,13 @@ for action, sequence in product(actions, range(sequences)):
     landmarks.append(temp)
     labels.append(label_map[action])
 
+# Convert landmarks and labels to numpy arrays
 X, Y = np.array(landmarks), to_categorical(labels).astype(int)
 
+# Split the data into training and testing sets
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.10, random_state=34, stratify=Y)
 
+# Define the model architecture
 model = Sequential()
 model.add(LSTM(32, return_sequences=True, activation='relu', input_shape=(10,126)))
 model.add(LSTM(64, return_sequences=True, activation='relu'))
@@ -38,12 +50,18 @@ model.add(LSTM(32, return_sequences=False, activation='relu'))
 model.add(Dense(32, activation='relu'))
 model.add(Dense(actions.shape[0], activation='softmax'))
 
+# Compile the model with Adam optimizer and categorical cross-entropy loss
 model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+# Train the model
 model.fit(X_train, Y_train, epochs=100)
 
+# Save the trained model
 model.save('my_model')
 
+# Make predictions on the test set
 predictions = np.argmax(model.predict(X_test), axis=1)
+# Get the true labels from the test set
 test_labels = np.argmax(Y_test, axis=1)
 
+# Calculate the accuracy of the predictions
 accuracy = metrics.accuracy_score(test_labels, predictions)
