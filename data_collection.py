6ad2531d1abd8196a1460f0bece36c1238a58236
Dmitrii Govor <85813394+dgovor@users.noreply.github.com>
2023-06-26 17:03:01 -0700

    Comments

--- data_collection.py
+++ data_collection.py
@@ -34,7 +34,9 @@ if not cap.isOpened():
 
 # Create a MediaPipe Holistic object for hand tracking and landmark extraction
 with mp.solutions.holistic.Holistic(min_detection_confidence=0.75, min_tracking_confidence=0.75) as holistic:
+    # Loop through each action, sequence, and frame to record data
     for action, sequence, frame in product(actions, range(sequences), range(frames)):
+        # If it is the first frame of a sequence, wait for the spacebar key press to start recording
         if frame == 0: 
             while True:
                 if keyboard.is_pressed(' '):
@@ -50,25 +52,33 @@ with mp.solutions.holistic.Holistic(min_detection_confidence=0.75, min_tracking_
                 cv2.putText(image, 'Press "Space" when you are ready.', (20,450), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2, cv2.LINE_AA)
                 cv2.imshow('Camera', image)
                 cv2.waitKey(1)
-
+                
+                # Check if the 'Camera' window was closed and break the loop
                 if cv2.getWindowProperty('Camera',cv2.WND_PROP_VISIBLE) < 1:
                     break
         else:
+            # For subsequent frames, directly read the image from the camera
             _, image = cap.read()
+            # Process the image and extract hand landmarks using the MediaPipe Holistic pipeline
             results = image_process(image, holistic)
+            # Draw the hand landmarks on the image
             draw_landmarks(image, results)
 
+            # Display text on the image indicating the action and sequence number being recorded
             cv2.putText(image, 'Recroding data for the "{}". Sequence number {}.'.format(action, sequence),
                         (20,20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1, cv2.LINE_AA)
             cv2.imshow('Camera', image)
             cv2.waitKey(1)
-        
+
+        # Check if the 'Camera' window was closed and break the loop
         if cv2.getWindowProperty('Camera',cv2.WND_PROP_VISIBLE) < 1:
              break
 
+        # Extract the landmarks from both hands and save them in arrays
         keypoints = keypoint_extraction(results)
         frame_path = os.path.join(PATH, action, str(sequence), str(frame))
         np.save(frame_path, keypoints)
 
+    # Release the camera and close any remaining windows
     cap.release()
     cv2.destroyAllWindows()
