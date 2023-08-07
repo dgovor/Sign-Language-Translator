a424cc10fb444e68ecaef45960de9cc2a18a03d9
Dmitrii Govor <85813394+dgovor@users.noreply.github.com>
2023-08-07 16:09:03 -0700

    New version on the main file

--- main.py
+++ main.py
@@ -3,23 +3,28 @@
 # Import necessary libraries
 import numpy as np
 import os
+import string
 import mediapipe as mp
 import cv2
-import keyboard
 from my_functions import *
+import keyboard
 from tensorflow.keras.models import load_model
+from gingerit.gingerit import GingerIt
 
 # Set the path to the data directory
 PATH = os.path.join('data')
 
-# Create an array of actions (signs) labels by listing the contents of the data directory
+# Create an array of action labels by listing the contents of the data directory
 actions = np.array(os.listdir(PATH))
 
 # Load the trained model
 model = load_model('my_model')
 
-# Initialize the sentence and keypoints (landmarks) lists
-sentence, keypoints = [' '], []
+# Create an instance of the GingerIt grammar correction tool
+parser = GingerIt()
+
+# Initialize the lists
+sentence, keypoints, last_prediction, grammar, grammar_result = [], [], [], [], []
 
 # Access the camera and check if the camera is opened successfully
 cap = cv2.VideoCapture(0)
@@ -51,30 +56,66 @@ with mp.solutions.holistic.Holistic(min_detection_confidence=0.75, min_tracking_
 
             # Check if the maximum prediction value is above 0.9
             if np.amax(prediction) > 0.9:
-                # Check if the predicted sign is different from the previous sign in the sentence
-                if sentence[-1] != actions[np.argmax(prediction)]:
+                # Check if the predicted sign is different from the previously predicted sign
+                if last_prediction != actions[np.argmax(prediction)]:
                     # Append the predicted sign to the sentence list
                     sentence.append(actions[np.argmax(prediction)])
+                    # Record a new prediction to use it on the next cycle
+                    last_prediction = actions[np.argmax(prediction)]
 
         # Limit the sentence length to 7 elements to make sure it fits on the screen
         if len(sentence) > 7:
             sentence = sentence[-7:]
 
-        # Reset the sentence if the spacebar is pressed
+        # Reset if the "Spacebar" is pressed
         if keyboard.is_pressed(' '):
-            sentence = [' ']
+            sentence, keypoints, last_prediction, grammar, grammar_result = [], [], [], [], []
 
-        # Calculate the size of the text to be displayed and the X coordinate for centering the text on the image
-        textsize = cv2.getTextSize(' '.join(sentence), cv2.FONT_HERSHEY_SIMPLEX, 1, 2)[0]
-        text_X_coord = (image.shape[1] - textsize[0]) // 2
+        # Check if the list is not empty
+        if sentence:
+            # Capitalize the first word of the sentence
+            sentence[0] = sentence[0].capitalize()
 
-        # Draw the sentence on the image
-        cv2.putText(image, ' '.join(sentence), (text_X_coord, 470), 
-                       cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        # Check if the sentence has at least two elements
+        if len(sentence) >= 2:
+            # Check if the last element of the sentence belongs to the alphabet (lower or upper cases)
+            if sentence[-1] in string.ascii_lowercase or sentence[-1] in string.ascii_uppercase:
+                # Check if the second last element of sentence belongs to the alphabet or is a new word
+                if sentence[-2] in string.ascii_lowercase or sentence[-2] in string.ascii_uppercase or (sentence[-2] not in actions and sentence[-2] not in list(x.capitalize() for x in actions)):
+                    # Combine last two elements
+                    sentence[-1] = sentence[-2] + sentence[-1]
+                    sentence.pop(len(sentence) - 2)
+                    sentence[-1] = sentence[-1].capitalize()
+
+        # Perform grammar check if "Enter" is pressed
+        if keyboard.is_pressed('enter'):
+            # Record the words in the sentence list into a single string
+            text = ' '.join(sentence)
+            # Parse the text to check and correct grammar using the parser instance
+            grammar = parser.parse(text)
+            # Extract the corrected result from the parsed grammar
+            grammar_result = grammar['result']
+
+        if grammar_result:
+            # Calculate the size of the text to be displayed and the X coordinate for centering the text on the image
+            textsize = cv2.getTextSize(grammar_result, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)[0]
+            text_X_coord = (image.shape[1] - textsize[0]) // 2
+
+            # Draw the sentence on the image
+            cv2.putText(image, grammar_result, (text_X_coord, 470),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        else:
+            # Calculate the size of the text to be displayed and the X coordinate for centering the text on the image
+            textsize = cv2.getTextSize(' '.join(sentence), cv2.FONT_HERSHEY_SIMPLEX, 1, 2)[0]
+            text_X_coord = (image.shape[1] - textsize[0]) // 2
+
+            # Draw the sentence on the image
+            cv2.putText(image, ' '.join(sentence), (text_X_coord, 470),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
         # Show the image on the display
         cv2.imshow('Camera', image)
-        
+
         cv2.waitKey(1)
 
         # Check if the 'Camera' window was closed and break the loop
